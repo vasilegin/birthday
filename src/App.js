c42a58fcec6992b80c7df442076134c6d94116ab
VladVasilegin <Vasilegin@gmail.com>
2022-10-15 21:16:59 +0300

    Made last changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import get_current_age from "./utils/currentAge";
 import numToRoman from "./utils/numToRoman";
 import "@pqina/flip/dist/flip.min.css";
 import CountDown from "./CountDown/CountDown";
+import bax from "./images/bax.png"
+import egg from "./images/egg.png"
+import xlop from "./images/xlop.png"
 
 let date_of_birthday = new Date("1999-07-02");
 let month_of_birthday = date_of_birthday.getMonth();
@@ -15,6 +18,12 @@ let next_date_of_birthday = new Date(new Date().getFullYear() + 1, month_of_birt
 function App() {
 return (
   <div className="App">
+    <img src= {bax} alt="Праздничные картинки"/>
+    <img src= {egg} alt="Праздничные картинки"/>
+    <img src= {xlop} alt="Праздничные картинки"/>
+    <img src= {egg} alt="Праздничные картинки"/>
+    <img src= {bax} alt="Праздничные картинки"/>
+    <div className="empty"></div>
     <div className="age">{numToRoman(get_current_age(date_of_birthday))}</div>
     <div className="empty"></div>
     <div className="timer_wrap">
--- src/utils/currentAge.js
+++ src/utils/currentAge.js
@@ -2,4 +2,4 @@ function get_current_age(date) {
 return ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
 }
 
-export default get_current_age
+export default get_current_age;
